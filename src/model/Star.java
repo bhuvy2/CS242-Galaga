425addd9230a87af83741d9ea253775a993aded0
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2017-04-17 10:41:57 -0500

    Updated star with comments

--- src/model/Star.java
+++ src/model/Star.java
@@ -8,13 +8,14 @@ import model.ships.Alien;
 import display.view.GameWindow;
 
 /**
- * This is for the Galaga Project in APCS
  * The Class Star s a star to be painted.
  * @author Bhuvan Venkatesh
- * @since 5/31/13
  */
 public class Star extends GameSprite
 {
+	/**
+	 * Colors that the star could take on
+	 */
 	private static final Color[] colors = 
 		{Color.BLACK, Color.BLUE, Color.CYAN, 
 			Color.DARK_GRAY, Color.GRAY, 
@@ -25,27 +26,34 @@ public class Star extends GameSprite
 	private int count;
 	public static final int length = 1;
 	
-	public Star(int toRight, int toEdge){
-		super("");
-		this.x = toRight;
-		this.y = toEdge;
-		count = 0;
-		image = null;
-		setStarColor(colors[(int) (Math.random()*colors.length)]);
+	/**
+	 * @param x, the x position of the start
+	 * @param y, the y position of the start
+	 */
+	public Star(int x, int y){
+		this(x, y, colors[(int) (Math.random()*colors.length)]);
 	}
 	
 
-	public Star(int toRight, int toEdge, Color a){
+	/**
+	 * Constructs a start with a specific color
+	 * @param x
+	 * @param y
+	 * @param a
+	 */
+	public Star(int x, int y, Color a){
 		super("");
-		this.x = toRight; //IN THIS CASE TO RIGHT ARE PIXELS NOT 1 TO 100
-		this.y = toEdge; //SAME
+		this.x = x;
+		this.y = y;
 		count = 0;
 		setStarColor(a);
 		image = null;
 	}
 	
-	public void move()
-	{
+	/**
+	 * Moves the alien given a delay
+	 */
+	public void move(){
 		if(count%(Alien.DELAY) == 0)
 			this.y++;
 		if(this.y > GameWindow.BOARD_HEIGHT)
@@ -53,8 +61,10 @@ public class Star extends GameSprite
 		count++;
 	}
 	
-	public void fade()
-	{
+	/**
+	 * Fades the star randomly
+	 */
+	public void fade(){
 		if(!(count%(Alien.DELAY*2) == 0))
 			return;
 		
@@ -81,21 +91,33 @@ public class Star extends GameSprite
 		
 	}
 	
+	/* (non-Javadoc)
+	 * @see model.superclasses.GameSprite#tick()
+	 */
 	public void tick(){
 		this.move();
 		if (Math.random() > .5)
 			this.fade();
 	}
 	
+	/* (non-Javadoc)
+	 * @see model.superclasses.GameSprite#drawSelf(java.awt.Component, java.awt.Graphics)
+	 */
 	public void drawSelf(Component c, Graphics g){
 		g.setColor(starColor);
 		g.drawRect(x, y, Star.length, Star.length);
 		g.fillRect(x, y, Star.length, Star.length);
 	}
 
+	/**
+	 * @return The color of the star
+	 */
 	public Color getStarColor() {
 		return starColor;
 	}
+	/**
+	 * @param starColor, sets the color to what the star has
+	 */
 	public void setStarColor(Color starColor) {
 		this.starColor = starColor;
 	}
